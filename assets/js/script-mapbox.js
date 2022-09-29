59c258db0fad3e3ed57c4476ae75657cdf54031e
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2022-09-29 12:52:09 +0200

    gibgab style

--- assets/js/script-mapbox.js
+++ assets/js/script-mapbox.js
@@ -3,50 +3,27 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZ2liZ2FiIiwiYSI6ImNsOG11cDBhODBhZDQzdW9hYjF5b
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
-    center: [2.3483915,48.8534951], // starting position [lng, lat]
-    zoom: 2, // starting zoom
-    projection: 'mercator' // display the map as a 3D globe
+    style: 'mapbox://styles/gibgab/cl8mxqbqy00ai16piczjoxpmz?optimize=true', // style URL
+    center: [2.27587,48.85407], // starting position [lng, lat]
+    zoom: 13, // starting zoom
+    projection: 'mercator', // display the map as a 3D globe
+    optimizeForTerrain : true
     });
  
 map.on('style.load', () => {
    
 });
 
-const geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [2.3483915,48.8534951]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'Paris'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [2.3483,48.8534]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'Paris Test'
-        }
-      }
-    ]
-  };
+const bounds = [
+    [2.27587,48.85407],
+    [2.27572,48.85370]
+  ];
 
-  // add markers to map
-for (const feature of geojson.features) {
-    // create a HTML element for each feature
-    const el = document.createElement('div');
-    el.className = 'marker';
-  
-    // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
-  }
+new mapboxgl.Marker().setLngLat([2.27587,48.85407]).addTo(map);
+
+map.addControl(
+    new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+    );
