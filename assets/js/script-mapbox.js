0a37668a42b816911c53acd97c6058e675760d31
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2022-10-03 12:56:39 +0200

    feu Rouge clusters

--- assets/js/script-mapbox.js
+++ assets/js/script-mapbox.js
@@ -214,7 +214,7 @@ function animateMarker(timestamp) {
 function animateForEach(timestamp,idVehicule) {
 
   const vitesse = 0.000001;
-  var temps = Math.floor(timestamp/100);
+  var temps = Math.floor(timestamp/200);
   var end = false;
   
 
@@ -230,9 +230,6 @@ function animateForEach(timestamp,idVehicule) {
       if(difCoordsX != 0 || difCoordsY != 0)
       {
         var nb = Math.sqrt((difCoordsX*difCoordsX) + (difCoordsY*difCoordsY))*10000;
-        console.log(nb);
-        //var nb = (difCoordsX+difCoordsY)*10000;
-
         if(nb > 1 || nb < -1)
         {
           for(var z = 0; z < nb ; z++)
@@ -257,3 +254,80 @@ function animateForEach(timestamp,idVehicule) {
 
 }
 
+
+
+
+
+/*CLUSTERS */
+
+map.on('load', () => {
+  // Add a new source from our GeoJSON data and
+  // set the 'cluster' option to true. GL-JS will
+  // add the point_count property to your source data.
+  map.addSource('earthquakes', {
+  type: 'geojson',
+  // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+  // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+  data: '././signalisation-tricolore.geojson',
+  cluster: true,
+  clusterMaxZoom: 14, // Max zoom to cluster points on
+  clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+  });
+   
+  map.addLayer({
+  id: 'clusters',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['has', 'point_count'],
+  paint: {
+  // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+  // with three steps to implement three types of circles:
+  //   * Blue, 20px circles when point count is less than 100
+  //   * Yellow, 30px circles when point count is between 100 and 750
+  //   * Pink, 40px circles when point count is greater than or equal to 750
+  'circle-color': [
+  'step',
+  ['get', 'point_count'],
+  '#51bbd6',
+  100,
+  '#f1f075',
+  750,
+  '#f28cb1'
+  ],
+  'circle-radius': [
+  'step',
+  ['get', 'point_count'],
+  20,
+  100,
+  30,
+  750,
+  40
+  ]
+  }
+  });
+   
+  map.addLayer({
+  id: 'cluster-count',
+  type: 'symbol',
+  source: 'earthquakes',
+  filter: ['has', 'point_count'],
+  layout: {
+  'text-field': '{point_count_abbreviated}',
+  'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+  'text-size': 12
+  }
+  });
+   
+  map.addLayer({
+  id: 'unclustered-point',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['!', ['has', 'point_count']],
+  paint: {
+  'circle-color': '#11b4da',
+  'circle-radius': 4,
+  'circle-stroke-width': 1,
+  'circle-stroke-color': '#fff'
+  }
+  });
+});
